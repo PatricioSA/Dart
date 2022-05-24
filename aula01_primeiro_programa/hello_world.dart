1f12e48d214a53fc146f92a1655fc0f6a862042a
Patricio Samuel <patricioanjosjf@gmail.com>
2022-05-23 21:41:36 -0300

    aula-23-05

---
+++ aula01_primeiro_programa/hello_world.dart
@@ -0,0 +1,8 @@
+//Curso de Tecnologia da Informação
+//Primeiro programa em Dart: Hello World!
+
+void main() {
+  //Primeiro programa em Dart
+  //Printando no terminal com Code Runner
+  print('Hello World!!!');
+}
