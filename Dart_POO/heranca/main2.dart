1e899a91ad173e944d3f986b0f507c207af24894
Patricio Samuel <patricioanjosjf@gmail.com>
2022-06-30 19:13:30 -0300

    aula-29-06

--- Dart_POO/heranca/main2.dart
+++ Dart_POO/heranca/main2.dart
@@ -3,4 +3,17 @@ import 'model/animal2.dart';
 void main() {
   Cachorro toto = new Cachorro();
   Gato felix = new Gato();
+
+  //Definindo os valores dos atributos
+  toto.cor = 'Marrom';
+  felix.peso = 3.5;
+
+  print('A cor do cachorro é: ${toto.cor}');
+  toto.latir();
+  print('O peso do gato é: ${felix.peso}');
+  felix.miar();
+  print('-----------------------------------');
+  toto.farejar();
+  felix.farejar();
+  print('-----------------------------------');
 }
